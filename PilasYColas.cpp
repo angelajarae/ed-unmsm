946311bdd6a6c57b41d67e9e72ed78894ce10c72
angelajarae <angelajaraespinoza@gmail.com>
2024-05-12 00:29:51 -0500

    Correcion PilasYColas.cpp

--- PilasYColas.cpp
+++ PilasYColas.cpp
@@ -10,6 +10,160 @@ struct nodo{
 };
 
 typedef struct nodo *TpLista; // Crear un nuevo tipo de dato de forma puntero
+int nSobresRecibidos=0;
+
+//**************GENERACION DE RANDOMS *************
+string generarTipoEnvio() {
+    srand(time(0));
+    int n=rand() % 2 + 1;
+
+    if(n==1){
+    	return "Nacional";
+	}
+    else{
+    	return "Internacional";
+	}
+}
+int generarCodigo() {
+    srand(time(0));
+    return rand() % 99 + 1;
+}
+
+
+//**************FUNCIONES DE LISTAS *************
+TpLista CrearNodo(){
+	TpLista sobre=new(struct nodo);
+	sobre->codigo=generarCodigo();
+	sobre->tipoEnvio=generarTipoEnvio();
+	sobre->sgte=NULL;
+	return sobre;
+}
+void VerLista(TpLista l){
+	TpLista lista = l;
+	int i=0;
+	while(lista != NULL){
+		cout<<" "<<i+1<<") "<<lista->codigo<<":"<<lista->tipoEnvio<<endl;
+		lista=lista->sgte;
+		i++;
+	}
+
+}
+
+
+//**************FUNCIONES DE COLAS *************
+// FUncion Insertar al FInal (ENCOLAR)
+void Encolar(TpLista &cola, TpLista nodo){
+	TpLista nuevo=nodo, p=cola;
+	//ir al final de la ista
+
+	if(cola == NULL)
+		cola = nuevo;
+	else{
+		while(p->sgte != NULL)
+			p=p->sgte;
+		p->sgte =nuevo;
+
+	}
+}
+// FUncion DESENCOLAR elimina el primer elemento
+void Desencolar(TpLista &cola){
+	TpLista t=cola;
+
+	if(cola==NULL){
+		cout<<"\nCola esta vacia"<<endl<<endl;
+	}
+	else if(cola->sgte==NULL){
+		cout<<"\nNodo eliminado es "<<t->codigo<<":"<<t->tipoEnvio<<endl<<endl;
+		cola=NULL;
+		delete(t);
+	}
+	else{
+		cout<<"\nNodo eliminado es "<<t->codigo<<":"<<t->tipoEnvio<<endl<<endl;
+		cola=cola->sgte;
+		delete(t);
+	}
+	
+}
+
+
+
+//**************FUNCIONES DE PILAS *************
+// POP extrae un nodo desde el inicio de una PILA
+TpLista Pop(TpLista &lista){
+	TpLista t=lista;
+	if(lista == NULL){
+		cout<<"ERROR: Lista vacia.. no permite esta opcion"<<endl;
+		system("pause");
+		return t;
+	}
+	else if(lista->sgte==NULL){
+		lista=NULL;
+		t->	sgte=NULL;
+		return t;
+	}
+	else{
+		lista=lista->sgte;
+		t->	sgte=NULL;
+		return t;
+	}
+	
+}
+void Push(TpLista &lista, TpLista q){
+	if(lista !=NULL){
+		q->sgte=lista;
+	}
+	lista =q;
+}
+
+
+
+//**************OPCIONES *************
+void registrarSobre(TpLista &pilaSobres,int &nSobresRecibidos){
+	if(nSobresRecibidos<8){
+		Push(pilaSobres,CrearNodo());
+	}
+	else{
+		cout<<"No se pueden recibir mas sobres"<<endl;
+	}
+
+	nSobresRecibidos++;
+}
+
+
+void distribuirSobres(TpLista &pilaSobres,TpLista &colaNacionales,TpLista &colaInternacionales){
+    if(pilaSobres!=NULL){
+        TpLista sobre=Pop(pilaSobres);
+
+        if(sobre->tipoEnvio=="Nacional"){
+            Encolar(colaNacionales,sobre);
+			nSobresRecibidos--;
+        }
+        else{
+            Encolar(colaInternacionales,sobre);
+			nSobresRecibidos--;
+        }
+    }
+}
+void enviarSobre(TpLista &cola){
+    Desencolar(cola);
+}
+
+void mostrarListas(TpLista pilaSobres,TpLista colaNacionales,TpLista colaInternacionales){
+	cout<<"*******SOBRES RECIBIDOS **********"<<endl<<endl;
+
+    VerLista(pilaSobres);
+
+
+
+	cout<<"*******ENVIOS NACIONALES **********"<<endl<<endl;
+
+    VerLista(colaNacionales);
+
+
+	cout<<"*******ENVIOS INTERNACIONALES  **********"<<endl<<endl;
+
+    VerLista(colaInternacionales);
+}
 
 
 
@@ -25,145 +179,9 @@ void menu(){
 
 	cout<<"Ingresar Opcion---> "<<endl;
 }
-
-
-void verLista(TpLista l){
-	TpLista lista = l;
-	int i=0;
-	while(lista != NULL){
-		cout<<" "<<i+1<<") "<<lista->codigo<<":"<<lista->tipoEnvio<<endl;
-		lista=lista->sgte;
-		i++;
-	}
-
-}
-
-
-//**************FUNCIONES DE COLAS *************
-// FUncion Insertar al FInal (ENCOLAR)
-void encolar(TpLista &cola, TpLista nodo){
-	TpLista nuevo=nodo, p=cola;
-	//ir al final de la ista
-
-	if(cola == NULL)
-		cola = nuevo;
-	else{
-		while(p->sgte != NULL)
-			p=p->sgte;
-		p->sgte =nuevo;
-
-	}
-}
-// FUncion DESENCOLAR elimina el primer elemento
-void desencolar(TpLista &cola){
-	TpLista t=cola;
-	cola=cola->sgte;
-	cout<<"\nNodo eliminado es "<<t->codigo<<":"<<t->tipoEnvio<<endl<<endl;
-	delete(t);
-}
-
-
-
-
-//**************FUNCIONES DE PILAS *************
-// POP extrae un nodo desde el inicio de una PILA
-TpLista Pop(TpLista &lista){
-	TpLista t=lista;
-	if(lista == NULL){
-		cout<<"ERROR: Lista vacia.. no permite esta opcion"<<endl;
-		system("pause");
-		return t;
-	}
-	lista=lista->sgte;
-	t->	sgte=NULL;
-	return t;
-}
-
-void Push(TpLista &lista, TpLista q){
-	if(lista !=NULL){
-		q->sgte=lista;
-	}
-	lista =q;
-
-}
-
-//**************GENERACION DE RANDOMS *************
-string generarTipoEnvio() {
-    srand(time(0));
-    int n=rand() % 2 + 1;
-
-    if(n==1){
-    	return "Nacional";
-	}
-    else{
-    	return "Internacional";
-	}
-}
-
-
-int generarCodigo() {
-    srand(time(0));
-    return rand() % 99 + 1;
-}
-
-//**************OPCIONES *************
-void registrarSobre(TpLista &pilaSobres,int &nSobresRecibidos){
-	if(nSobresRecibidos<8){
-		TpLista sobre=new(struct nodo);
-		sobre->codigo=generarCodigo();
-		sobre->tipoEnvio=generarTipoEnvio();
-        sobre->sgte=NULL;
-		Push(pilaSobres,sobre);
-	}
-	else{
-		cout<<"No se pueden recibir m�s sobres"<<endl;
-	}
-
-	nSobresRecibidos++;
-}
-
-
-void distribuirSobres(TpLista &pilaSobres,TpLista &colaNacionales,TpLista &colaInternacionales){
-    while(pilaSobres!=NULL){
-        TpLista sobre=Pop(pilaSobres);
-
-        if(sobre->tipoEnvio=="Nacional"){
-            encolar(colaNacionales,sobre);
-        }
-        else{
-            encolar(colaInternacionales,sobre);
-        }
-    }
-}
-void enviarSobre(TpLista &cola){
-    desencolar(cola);
-}
-
-void mostrarListas(TpLista pilaSobres,TpLista colaNacionales,TpLista colaInternacionales){
-	cout<<"*******SOBRES RECIBIDOS **********"<<endl<<endl;
-
-    verLista(pilaSobres);
-
-
-
-	cout<<"*******ENVIOS NACIONALES **********"<<endl<<endl;
-
-    verLista(colaNacionales);
-
-
-	cout<<"*******ENVIOS INTERNACIONALES  **********"<<endl<<endl;
-
-    verLista(colaInternacionales);
-
-
-
-}
-
-
 int main(){
 
 	TpLista pilaSobres=NULL,colaNacionales=NULL,colaInternacionales=NULL;
-	int nSobresRecibidos=0;
 	int opc;
 
 	do{
